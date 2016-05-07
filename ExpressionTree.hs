c201c946c6b020e09a79e9595cb26e7e8b24044a
Sven Schmidt <svenschmidt75@googlemail.com>
2016-05-07 08:00:42 -0600

    minor changes

--- ExpressionTree.hs
+++ ExpressionTree.hs
@@ -10,13 +10,25 @@ data Op = Add
         | Div
         deriving (Show)
 
+{- Input:
+ - 1. List of integers to create a syntax tree for
+ - 2. List of syntax trees generated, initially empty
+ - Output:
+ - List of all generated syntax trees
+-}
 root :: [Int] -> [Tree] -> [Tree]
 root       []        _ = error "Should never happen"
 root      [x] subtrees = let sta = Leaf x : subtrees in
                          let sts = Leaf (-x) : subtrees in
                          sta ++ sts
-root (x:y:xs) subtrees = let sta = map (\z -> Node Add (Leaf x) z) add_subtrees in
-                         let stb = map (\z -> Node Add (Leaf (-x)) z) add_subtrees in
-                         sta ++ stb
-                         where
-                            add_subtrees = root (y:xs) subtrees
+root (x:y:xs) trees = let sta1 = map (\z -> Node Add (Leaf x) z) subtrees in
+                      let sta2= map (\z -> Node Add (Leaf (-x)) z) subtrees in
+                      let stb1= map (\z -> Node Sub (Leaf x) z) subtrees in
+                      let stb2= map (\z -> Node Sub (Leaf (-x)) z) subtrees in
+                      let stc1= map (\z -> Node Mul (Leaf x) z) subtrees in
+                      let stc2= map (\z -> Node Mul (Leaf (-x)) z) subtrees in
+                      let std1= map (\z -> Node Div (Leaf x) z) subtrees in
+                      let std2= map (\z -> Node Div (Leaf (-x)) z) subtrees in
+                      sta1 ++ sta2 ++ stb1 ++ stb2 ++ stc1 ++ stc2 ++ std1 ++ std2
+                      where
+                        subtrees = root (y:xs) trees
